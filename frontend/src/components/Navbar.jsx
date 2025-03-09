772ae833671fd2577cedddc082a0639615f9eeca
Prakhar Srivastava <mathsmaticprakhar@gmail.com>
2025-03-09 14:47:19 +0530

    Initial commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; 
+import { useAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+const Navbar = () => {
+    const navigate=useNavigate();
+    const [searchQuery, setSearchQuery] = useState('');
+    const { isLoggedIn, logout } = useAuth();
+    const [menuOpen, setMenuOpen] = useState(false); // State to manage mobile menu
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        if (searchQuery.trim()) {
+            navigate(`/search?query=${searchQuery}`);
+        }
+    };
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen); // Toggle the menu open/close
+    };
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <Link to="/" className="brand-name">ElevateKicks</Link>
+            </div>
+
+            {/* Hamburger Menu Icon for Mobile */}
+            <div className="menu-icon" onClick={toggleMenu}>
+                <div className={menuOpen ? 'bar open' : 'bar'}></div>
+                <div className={menuOpen ? 'bar open' : 'bar'}></div>
+                <div className={menuOpen ? 'bar open' : 'bar'}></div>
+            </div>
+
+            {/* Navbar links */}
+            <div className={`navbar-links ${menuOpen ? 'active' : ''}`}>
+                <Link to="/" className="nav-item">Home</Link>
+                <Link to="/products" className="nav-item">Products</Link>
+                {isLoggedIn ? (
+                    <Link to='/' className='nav-item' onClick={logout}>Logout</Link>
+                ) : (
+                    <Link to="/login" className='nav-item'>Login</Link>
+                )}
+                <Link to="/cart" className="nav-item">Cart</Link>
+
+                {/* Search Form */}
+                <form className="search-form" onSubmit={handleSearch}>
+                    <input 
+                        type="text"
+                        className="search-input" 
+                        placeholder="Search products"
+                        value={searchQuery} 
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    />
+                </form>
+                
+                <Link to="/seller" className="nav-item seller-btn">Become a Seller</Link>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
+
+      
