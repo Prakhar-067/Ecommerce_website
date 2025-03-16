f2ee3828e731619285082477c1e2aae82fd37b32
Prakhar Srivastava <mathsmaticprakhar@gmail.com>
2025-03-16 22:42:39 +0530

    Last AND

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Home Page!');
 });
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
